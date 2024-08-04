3ae2c80dcf93639dab58978b9ec098543098df59
davidteixeira832001 <46758822+davidteixeira832001@users.noreply.github.com>
2024-08-04 18:50:21 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function subscribe() {
+    alert('Obrigado por subscrever! Você será redirecionado para o pagamento.');
+    // Aqui você pode adicionar a lógica para redirecionar para uma página de pagamento
+}
